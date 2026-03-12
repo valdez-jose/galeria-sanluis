7cd51cc584e08b2aa7f8ff1b798bd96209ae3245
valdez-jose <tu-email-de-github@example.comclear>
2026-03-12 03:32:59 -0300

    Creamos archivo imagen.ts y tambien los imagenes

---
+++ src/assets/imagenes.ts
@@ -0,0 +1,13 @@
+
+import plaza_pringles from './plaza_pringles.jpg';
+import plaza_pringles2 from './plaza_pringles2.jpg';
+import plaza_pringles3 from './plaza_pringles3.jpg';
+import san_luis_capital from './san_luis-capital.jpg';
+
+export{
+    plaza_pringles,
+    plaza_pringles2,
+    plaza_pringles3,
+    san_luis_capital
+}   
+
---
+++ src/data/turismo.ts
@@ -0,0 +1,21 @@
+
+export const lugaresTuristicos = [
+  {
+    id: 1,
+    nombre: "Potrero de los Funes",
+    imagen: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/c3/89/5e/hotel-potrero-de-los.jpg?w=900&h=500&s=1",
+    descripcion: "Un monumento simbólico que representa la bandera de la República Argentina."
+  },
+  {
+    id: 2,
+    nombre: "El trapiche",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqFKpNvhW1ZNBHhu7yjHzi1cZHRpes2-tRWg&s",
+    descripcion: "Espacio cultural que alberga exposiciones y actividades artísticas.",
+  },
+  {
+    id: 3,
+    nombre: "Villa Merlo",
+    imagen: "https://agenciasanluis.com/wp-content/uploads/2024/01/filo-630x381.jpg",
+    descripcion: "Espacio cultural que alberga exposiciones y actividades artísticas."
+  }
+];
